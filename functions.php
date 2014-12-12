d02369b1965b20b039f2a91db6f932c0857634dd
Jevon Wright <jevon@jevon.org>
2014-12-12 23:26:29 +1300

    Add print_exception_trace()

--- functions.php
+++ functions.php
@@ -83,3 +83,55 @@ function log_uncaught_exception($e) {
 
   return $q->execute($args);
 }
+
+function print_exception_trace($e) {
+  if (!$e) {
+    echo "<code>null</code>\n";
+    return;
+  }
+  if (!($e instanceof Exception)) {
+    echo "<i>Not exception: " . get_class($e) . ": " . print_r($e, true) . "</i>";
+    return;
+  }
+  echo "<ul>";
+  echo "<li><b>" . htmlspecialchars($e->getMessage()) . "</b> (<i>" . get_class($e) . "</i>)</li>\n";
+  echo "<li>" . htmlspecialchars($e->getFile()) . "#" . htmlspecialchars($e->getLine()) . "</li>\n";
+  foreach ($e->getTrace() as $e2) {
+    echo "<li>" . htmlspecialchars($e2['file']) . "#" . htmlspecialchars($e2['line']) . ": " . htmlspecialchars($e2['function']);
+    if (isset($e2['args'])) {
+      echo htmlspecialchars(format_exception_args_list($e2['args']));
+    }
+    echo "</li>\n";
+  }
+  if ($e->getPrevious()) {
+    echo "<li>Caused by:";
+    print_exception_trace($e->getPrevious());
+    echo "</li>";
+  }
+  echo "</ul>";
+}
+
+function format_exception_args_list($a, $count = 0) {
+  if (is_array($a)) {
+    $data = array();
+    $i = 0;
+    foreach ($a as $key => $value) {
+      if ($i++ >= 3) {
+        $data[] = "..."; break;
+      }
+      $data[$key] = format_exception_args_list($value);
+    }
+    $result = array();
+    foreach ($data as $value) {
+      if (is_object($value) && !method_exists($value, '__toString')) {
+        $result[] = get_class($value) . "@" . substr(spl_object_hash($value), -8);
+      } else if (is_string($value)) {
+        $result[] = "\"$value\"";
+      } else {
+        $result[] = $value;
+      }
+    }
+    return "(" . implode(", ", $result) . ")";
+  }
+  return $a;
+}
